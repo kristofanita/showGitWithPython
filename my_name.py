78db700f2d9989d86a25b5753d74fc22aeef9ebd
kristofanita <58989331+kristofanita@users.noreply.github.com>
2022-07-15 17:06:18 +0200

    added my_age

--- my_name.py
+++ my_name.py
@@ -1,4 +1,13 @@
-//This is my code
+#This is my code
 
 my_name="Anita"
 print(my_name)
+my_age=26
+print(my_age)
+
+
+#git add [file_name]
+#--> git add my_name.py
+#git commit -m "my_message"
+
+#git push origin main
